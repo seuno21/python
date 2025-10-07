1e3737309b0401d9d218d96b28f0dd8b5d38d766
Oluwaseun G Okuneye <oluwaseunokuneye@my.unt.edu>
2025-10-07 10:56:49 -0500

    practicing while loops

---
+++ whileloopprac.py
@@ -0,0 +1,27 @@
+
+#for loop= when we know exactly how many times we want to repeat.
+
+#while loop= when we want to repeat until a certain condition becomes false 
+
+
+"""syntax
+for i in range (): 1
+logic 
+
+while (condition):
+    number of logic """
+
+num = 1
+while num <= 5:
+    print(num)
+    num = num + 1
+
+
+"""task 1
+TASK 1 - Use a while loop to display numbers
+ from 1 through 10. """
+
+num = 1
+while num <= 10:
+    print(num)
+    num = num + 1
---
+++ whileloopprac2.py
@@ -0,0 +1,30 @@
+
+"""task 1
+TASK 1 - Use a while loop to display numbers
+ from 1 through 10. """
+
+num = 1
+while num <= 10:
+    print(num)
+    num = num + 1
+
+    """ TASK 2 - Display even numbers 
+    between 2 and 20 using while."""
+num = 2
+while num <= 20:
+    print(num)
+    num = num + 2
+print("odd number")
+num1 = 1
+while num1 <= 20:
+    print(num1)
+    num1 = num1 + 2
+
+"""Task 3: Countdown timer
+
+Goal: Print numbers from 5 down to 1 and then print “Blast off!"""
+num2 = 5
+while (5):
+    print(num2)
+    num2 = num2 - 1
+print("blast off")
