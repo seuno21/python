7ed728c4a4e378c732ddf996285b8cda66709068
Oluwaseun G Okuneye <oluwaseunokuneye@my.unt.edu>
2025-11-01 12:04:06 -0500

    Add files via upload
    practicing loops through lists

---
+++ loopsthroughlists.py
@@ -0,0 +1,36 @@
+
+cartlist = ["shoes", "bag", "dress", "watch"]
+
+for x in cartlist:
+    print(x)
+
+for i in range(len(cartlist)):
+    print(cartlist[i])
+
+#ascending 
+fruit = ["mango", "pineapple", "kiwi", "apple","grapes"]
+fruit.sort()
+print(fruit)
+
+number = [100,50,50,60,80]
+number.sort()
+print(number)
+
+#Descending order 
+fruit = ["mango", "pineapple", "kiwi", "apple","grapes"]
+fruit.sort(reverse = True)#Descending order 
+print(fruit)
+ 
+number = [100,50,50,60,80]
+number.sort(reverse = True)#Descending order
+print(number)
+
+fruit = ["mango", "pineapple", "kiwi", "apple","grapes"]
+fruit.reverse()
+print(fruit)
+
+
+
+
+
+
